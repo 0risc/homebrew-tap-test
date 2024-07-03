7f9a2aaadd08493092fa7357feb9c8e4d1e4b536
risc <148492865+0risc@users.noreply.github.com>
2024-07-03 19:18:34 +0000

    twc-go: add 1.1.0 bottle.

--- Formula/twc-go.rb
+++ Formula/twc-go.rb
@@ -11,6 +11,13 @@ class TwcGo < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    root_url "https://github.com/0risc/homebrew-tap-test/releases/download/twc-go-1.1.0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "70ec56712d55da62aab633401c14aca874e3d320cffe02b3ed2d5aa139a71b56"
+    sha256 cellar: :any_skip_relocation, ventura:      "4b5e4f1947eefacf59a5eb6be37813ee2d7c58748fd1e78ff224de90ac86ed30"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "10d2384bdf649a16fd24ca34f98f77e3cb567c30104761b052019763d4e0974b"
+  end
+
   depends_on "go" => :build
 
   def install
